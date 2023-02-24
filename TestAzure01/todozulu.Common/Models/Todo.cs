7d6812bdd05d970f4dfb12618b9a354fc8f0b49e
Pierre Oramas <pierre.oramas@syntegra2022.onmicrosoft.com>
2023-02-24 12:08:49 -0500

    cambios pendientes entity

---
+++ TestAzure01/todozulu.Common/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace todozulu.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string? TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
