778bbc10c21793a94ee99bf312232b8302682c5c
Pierre Oramas <pierre.oramas@syntegra2022.onmicrosoft.com>
2023-02-27 06:08:12 -0500

    Primera prueba OK, guardar en el storage

--- TestAzure01/todozulu.Functions/Entities/TodoEntity.cs
+++ TestAzure01/todozulu.Functions/Entities/TodoEntity.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.WindowsAzure.Storage.Table;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace todozulu.Functions.Entities
 {
-    public class TodoEntity:TableEntity
-    {        
+    public class TodoEntity : TableEntity
+    {
         public DateTime CreatedTime { get; set; }
         public string? TaskDescription { get; set; }
         public bool IsCompleted { get; set; }
